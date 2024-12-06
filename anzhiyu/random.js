d6d658073e4ff04ad4282f95bea67224ad202890
Xlhelp1 <6444991@gmail.com>
2024-12-06 20:29:18 +0800

    Site updated: 2024-12-06 20:29:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/05/我的网络生活新篇章/"];function toRandomPost(){
+var posts=["2024/12/06/免责声明/","2024/12/05/我的网络生活新篇章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
