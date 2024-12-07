2e62e7b1a48e30808539ba4ff70b908fcbd0b6ff
Xlhelp1 <6444991@gmail.com>
2024-12-07 10:43:32 +0800

    Site updated: 2024-12-07 10:43:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["p/6934b86c/","p/bb5dc064/"];function toRandomPost(){
+var posts=["p/6934b86c/","p/bb5dc064/","p/8ba628ad/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
