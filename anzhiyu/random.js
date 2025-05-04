17a97fcb611e20d69e3dd59d62bbf10942a1200e
xlhelp1 <6444991@gmail.com>
2025-05-04 14:41:04 +0800

    Site updated: 2025-05-04 14:41:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/03/hello-world/"];function toRandomPost(){
+var posts=["2025/05/04/新站新感悟/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
